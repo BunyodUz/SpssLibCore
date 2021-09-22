ab33d65cf96a7dadd42a306dd34d247c909573ae
Bunyod <bunyod.shahrixon@gmail.com>
2021-09-22 09:54:16 +0500

    Migration from SpssLib

---
+++ SpssLibCore/FileParser/Records/VariableDisplayInfo.cs
@@ -0,0 +1,15 @@
+﻿using SpssLibCore.SpssDataset;
+
+namespace SpssLibCore.FileParser.Records
+{
+    public class VariableDisplayInfo
+    {
+        public MeasurementType MeasurementType { get; internal set; }
+        public int Width { get; internal set; }
+        public Alignment Alignment { get; internal set; }
+
+        internal VariableDisplayInfo()
+        {
+        }
+    }
+}
---
+++ SpssLibCore/SpssDataset/DataType.cs
@@ -0,0 +1,17 @@
+﻿namespace SpssLibCore.SpssDataset
+{
+    /// <summary>
+    /// The SPSS data type
+    /// </summary>
+    public enum DataType
+    {
+        /// <summary>
+        /// Numeric data. Value should be a double (or null for SYSMISS)
+        /// </summary>
+        Numeric = 0,
+        /// <summary>
+        /// Text data. Value should be a string
+        /// </summary>
+        Text = 1
+    }
+}
